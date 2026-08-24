fdd230caa6ed599790708efad384c945a25074f5
Jordan Moore <jordanmoore753@gmail.com>
2026-08-24 15:03:29 -0400

    jsx: add SaveButton

---
+++ frontend/src/components/SaveButton.jsx
@@ -0,0 +1,12 @@
+function SaveButton() {
+  return (
+    <button
+      type="button"
+      className="rounded-md bg-gray-900 px-4 py-2 font-medium text-white shadow-sm hover:bg-gray-700"
+    >
+      Save
+    </button>
+  )
+}
+
+export default SaveButton
