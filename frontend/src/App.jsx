bc4531e3d8c7e0a9e52d5beab1ec71d223abf1e6
Jordan Moore <jordanmoore753@gmail.com>
2026-08-24 15:03:43 -0400

    jsx: track in flight requests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import MeasureForm from './components/MeasureForm'
 import MeasurementsList from './components/MeasurementsList'
+import SaveButton from './components/SaveButton'
 
 function App() {
   const [url, setUrl] = useState('https://www.jordanmoore.dev/')
   const [measurements, setMeasurements] = useState([])
+  const [isMeasuring, setIsMeasuring] = useState(false)
+  const [isMeasured, setIsMeasured] = useState(false)
 
   const handleMeasure = async (e) => {
     e.preventDefault()
+    setIsMeasured(false)
+    setMeasurements([])
+    setIsMeasuring(true)
 
     const response = await fetch('http://localhost:3000/captures', {
       method: 'POST',
@@ -23,6 +29,8 @@ function App() {
       { name: '500s', ...capture.count_500 },
       { name: 'Page Size', ...capture.total_size_mb, unit: 'MB' },
     ])
+    setIsMeasuring(false)
+    setIsMeasured(true)
   }
 
   return (
@@ -30,10 +38,16 @@ function App() {
       <h1 className="text-4xl font-semibold tracking-tight text-gray-900">
         SiteIQ
       </h1>
-      <MeasureForm url={url} onUrlChange={setUrl} onSubmit={handleMeasure} />
+      <MeasureForm
+        url={url}
+        onUrlChange={setUrl}
+        onSubmit={handleMeasure}
+        isMeasuring={isMeasuring}
+      />
       {measurements.length > 0 && (
         <MeasurementsList measurements={measurements} />
       )}
+      {isMeasured && <SaveButton />}
     </main>
   )
 }
