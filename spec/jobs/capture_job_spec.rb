64c22d74074961e8429cbcd8a08b58cc37ea016c
Jordan Moore <jordanmoore753@gmail.com>
2026-08-24 14:25:04 -0400

    spec: assert that lcp is captured

--- spec/jobs/capture_job_spec.rb
+++ spec/jobs/capture_job_spec.rb
@@ -1,16 +1,19 @@
 require "rails_helper"
 
 RSpec.describe CaptureJob do
-  it "returns the TTFB reported by the browser and quits it" do
+  it "returns the TTFB and LCP reported by the browser and quits it" do
     browser = instance_double(Ferrum::Browser, goto: true, quit: true)
     allow(Ferrum::Browser).to receive(:new).and_return(browser)
     allow(browser).to receive(:evaluate)
       .with("performance.getEntriesByType('navigation')[0].responseStart")
       .and_return(132.2)
+    allow(browser).to receive(:evaluate_async)
+      .with(CaptureJob::LCP_OBSERVER_JS, 5)
+      .and_return(268)
 
     result = CaptureJob.perform_now("https://www.jordanmoore.dev/")
 
-    expect(result).to eq(132.2)
+    expect(result).to eq(ttfb: 132.2, lcp: 268)
     expect(browser).to have_received(:goto).with("https://www.jordanmoore.dev/")
     expect(browser).to have_received(:quit)
   end
