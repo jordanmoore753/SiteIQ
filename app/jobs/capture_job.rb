9301f18268afaadb6cbf8b269aa5ef40aa6a3497
Jordan Moore <jordanmoore753@gmail.com>
2026-08-24 15:14:17 -0400

    job: remove computation of OK

--- app/jobs/capture_job.rb
+++ app/jobs/capture_job.rb
@@ -10,19 +10,13 @@ class CaptureJob < ApplicationJob
     setTimeout(() => arguments[0](null), 4000)
   JS
 
-  TTFB_OK_MS = 800
-  LCP_OK_MS = 2500
-  PAGE_SIZE_OK_MB = 1.5
-  ERROR_COUNT_OK = 0
-
-  private_constant :LCP_OBSERVER_JS, :TTFB_OK_MS, :LCP_OK_MS, :PAGE_SIZE_OK_MB, :ERROR_COUNT_OK
+  private_constant :LCP_OBSERVER_JS
 
   # Loads a URL in a headless browser and measures TTFB, LCP, subresource
   # error counts, and total page size.
   #
   # @param url [String] the URL to load and measure
-  # @return [Hash] a hash of metric name to { value:, ok: } for :ttfb, :lcp,
-  #   :count_404, :count_500, and :total_size_mb
+  # @return [Hash] :ttfb, :lcp, :count_404, :count_500, and :total_size_mb
   def perform(url)
     browser = Ferrum::Browser.new
     navigate(browser, url)
@@ -32,15 +26,13 @@ class CaptureJob < ApplicationJob
     responses = fetch_responses(browser)
     statuses = responses.map(&:status)
     total_size_mb = (responses.sum { |response| [ response.body_size || 0, 0 ].max } / 1024.0 / 1024.0).round(2)
-    count_404 = statuses.count(404)
-    count_500 = statuses.count(500)
 
     {
-      ttfb: { value: ttfb, ok: ttfb <= TTFB_OK_MS },
-      lcp: { value: lcp, ok: lcp.nil? || lcp <= LCP_OK_MS },
-      count_404: { value: count_404, ok: count_404 <= ERROR_COUNT_OK },
-      count_500: { value: count_500, ok: count_500 <= ERROR_COUNT_OK },
-      total_size_mb: { value: total_size_mb, ok: total_size_mb <= PAGE_SIZE_OK_MB },
+      ttfb: ttfb,
+      lcp: lcp,
+      count_404: statuses.count(404),
+      count_500: statuses.count(500),
+      total_size_mb: total_size_mb,
     }
   ensure
     browser&.quit
