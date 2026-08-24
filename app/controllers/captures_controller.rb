b0ff9be0b29606e616aa74ad5fd53724542702f7
Jordan Moore <jordanmoore753@gmail.com>
2026-08-24 14:11:26 -0400

    rb: add captures route and controller

---
+++ app/controllers/captures_controller.rb
@@ -0,0 +1,18 @@
+class CapturesController < ApplicationController
+  def create
+    site = Site.find_or_create_by(url: capture_params[:url])
+    capture = site.captures.new
+
+    if capture.save
+      render json: capture, status: :created
+    else
+      render json: capture.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def capture_params
+    params.require(:capture).permit(:url)
+  end
+end
