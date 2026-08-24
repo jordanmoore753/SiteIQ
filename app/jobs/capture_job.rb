965dcaa1a259664eab1b70c514b8ed5463e4baff
Jordan Moore <jordanmoore753@gmail.com>
2026-08-24 14:24:58 -0400

    job: add lcp to capture job

--- app/jobs/capture_job.rb
+++ app/jobs/capture_job.rb
@@ -1,10 +1,22 @@
 class CaptureJob < ApplicationJob
   queue_as :default
 
+  LCP_OBSERVER_JS = <<~JS
+    const po = new PerformanceObserver((list) => {
+      const entries = list.getEntries()
+      arguments[0](entries[entries.length - 1].startTime)
+    })
+    po.observe({ type: "largest-contentful-paint", buffered: true })
+  JS
+
   def perform(url)
     browser = Ferrum::Browser.new
     browser.goto(url)
-    browser.evaluate("performance.getEntriesByType('navigation')[0].responseStart")
+
+    ttfb = browser.evaluate("performance.getEntriesByType('navigation')[0].responseStart")
+    lcp = browser.evaluate_async(LCP_OBSERVER_JS, 5)
+
+    { ttfb: ttfb, lcp: lcp }
   ensure
     browser&.quit
   end
